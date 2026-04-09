06db29f087467bec6067952610fdd548c8270585
Zultrafury <zultrafury@gmail.com>
2026-04-09 04:52:31 -0600

    main pages + styling created

--- obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c738590c62f4031eb4c525836f84cc933624ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67b24413ded732a68bd6847a051a80cd95e7812")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
